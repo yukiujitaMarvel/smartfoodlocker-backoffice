cfd8b8c4b8e2d07b1f68a404eb79edbc77074d0a
[ujitayuki0329] <[ujitayuki0329@gmail.com]>
2022-06-30 10:57:22 +0900

    認証UI追加

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -35,8 +35,29 @@
           <v-list-item-content>
             <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
+
         </v-list-item>
       </v-list>
+
+      <div class="conpany-wrap">
+        <v-list-item class="px-2">
+          <v-list-item-avatar></v-list-item-avatar>
+          <v-list-item-title></v-list-item-title>
+          <amplify-sign-out></amplify-sign-out>
+        </v-list-item>
+        <div class="conpany-inner-wrap">
+          <v-list-item class="px-2">
+            <v-list-item-avatar>
+              <v-icon>mdi-home-city</v-icon>
+            </v-list-item-avatar>
+            <v-list-item-title>コマジャパン株式会社</v-list-item-title>
+            <v-btn icon>
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </v-list-item>
+        </div>
+      </div>
+
     </v-navigation-drawer>
   </v-card>
 </template>
@@ -66,6 +87,7 @@
 }
 .v-sheet.v-card {
   border-radius: 0px;
+  position: relative;
 }
 .v-card .v-sheet .theme--light{
   color: white;
@@ -83,6 +105,19 @@
 }
 .v-list-item__title {
   font-weight: bold !important;
+}
+.conpany-wrap {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 2px 2px 5px 2px;
+}
+.conpany-inner-wrap{
+  width: 100%;
+  border: 1px solid #ddd;
+}
+.conpany-inner-wrap .v-list-item__title{ 
+  font-size: 12px;
 }
 
 </style>
