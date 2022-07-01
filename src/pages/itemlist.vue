e410a192c6b31e88d3f14f0ae2d4b5745047abd3
[ujitayuki0329] <[ujitayuki0329@gmail.com]>
2022-07-01 12:12:49 +0900

    itemsからデータ取得

--- src/pages/itemlist.vue
+++ src/pages/itemlist.vue
@@ -42,31 +42,30 @@
               :items-per-page="5"
               class="elevation-1 my-3 mx-auto"
             >
-            <!-- サムネイル -->
-            <template v-slot:[`item.item_img`]="{ item }">
-                <v-img :src="item.item_img" 
-                        :aspect-ratop="16/9" 
-                        height="7vw" 
-                        min-height="100px"
-                        width="7vw" 
-                        min-width="100px" 
-                        class="ma-0 pa-0"
-                ></v-img>
-            </template>
+              <!-- サムネイル -->
+              <template v-slot:[`item.item_img`]="{ item }">
+                  <v-img :src="item.item_img" 
+                          :aspect-ratop="16/9" 
+                          height="7vw" 
+                          min-height="100px"
+                          width="7vw" 
+                          min-width="100px" 
+                          class="ma-0 pa-0"
+                  ></v-img>
+              </template>
 
-            
+              
 
-            <template v-slot:[`item.actions`]="{ item }">
-              <v-icon
-                nomal
-                @click="edit(item)"
-              >
-                mdi-dots-vertical
-              </v-icon>
-            </template>
+              <template v-slot:[`item.actions`]="{ item }">
+                <v-icon
+                  nomal
+                  @click="edit(item)"
+                >
+                  mdi-dots-vertical
+                </v-icon>
+              </template>
 
-            
-          </v-data-table>
+            </v-data-table>
           </v-card>
         </v-col>
       </v-row>
@@ -75,82 +74,85 @@
 </template>
 
 <script>
+import { API, graphqlOperation} from 'aws-amplify'
+import { listItems } from '../graphql/queries'
 import Sidebar from '~/components/Sidebar'
 import Modal from '~/components/Modal'
 import '~/assets/css/style.css'
 
-  export default {
-    head() {
-      return {
-        title: '商品一覧 | smartfoodlocker管理画面'
-      }
-    },
-    components: {
-      Sidebar,
-      Modal
-    },
-    data () {
-      return {
-        dialog: false,
-        search: '',
-        headers: [
-          {
-            text: '商品番号',
-            align: 'start',
-            // filterable: false,
-            value: 'name',
-          },
-          { text: '画像', value: 'item_img' },
-          { text: 'カテゴリ', value: 'category' },
-          { text: '商品名', value: 'item_name' },
-          { text: '値段(税込み)', value: 'item_price' },
-          { text: '備考', value: 'remarks' },
-          { text: '操作', value: 'actions' },
-        ],
-        desserts: [
-          {
-            name: 'p-6768',
-            item_img: require('@/assets/img/yakiniku.png'),
-            category: '弁当',
-            item_name: '焼肉弁当',
-            item_price: '600円',
-            remarks: 'オプション　ご飯　味噌汁',
-          },
-          {
-            name: 'p-6767',
-            item_img: require('@/assets/img/fish.png'),
-            category: '弁当',
-            item_name: '焼き魚弁当',
-            item_price: '580円',
-            remarks: 'オプション　ご飯　味噌汁',
-          },
-          {
-            name: 'p-6766',
-            item_img: require('@/assets/img/egg.png'),
-            category: '弁当',
-            item_name: 'たまごサンド',
-            item_price: '450円',
-            remarks: '-',
-          },
-          {
-            name: 'p-6765',
-            item_img: require('@/assets/img/soup.png'),
-            category: '弁当',
-            item_name: 'わかめスープ',
-            item_price: '380円',
-            remarks: '-',
-          },
-          
-          
-        ],
-      }
-    },
-    methods: {
-      edit(item) {
-        console.log(item.name);
-      },
+export default {
+  head() {
+    return {
+      title: '商品一覧 | smartfoodlocker管理画面'
     }
+  },
+  components: {
+    Sidebar,
+    Modal
+  },
+  data () {
+    return {
+      dialog: false,
+      search: '',
+      headers: [
+        {
+          text: '商品番号',
+          align: 'start',
+          value: 'item_id',
+        },
+        { text: '画像', value: 'item_img' },
+        { text: 'カテゴリ', value: 'category_id' },
+        { text: '商品名', value: 'item_name' },
+        { text: '値段(税込み)', value: 'item_price' },
+        { text: '備考', value: 'remarks' },
+        { text: '操作', value: 'actions' },
+      ],
+      desserts: [
+        // {
+        //   name: 'p-6768',
+        //   item_img: require('@/assets/img/yakiniku.png'),
+        //   category: '弁当',
+        //   item_name: '焼肉弁当',
+        //   item_price: '600円',
+        //   remarks: 'オプション　ご飯　味噌汁',
+        // },
+      ],
+    }
+  },
+  async created() {
+    await this.getItems();
+  },
+  methods: {
+    async getItems() {
+      const items = await API.graphql(graphqlOperation(listItems));
+      const itemLists = items.data.listItems.items;
+
+      var category_name1 = '弁当'
+      var category_name2 = 'サンドイッチ'
+      var category_name3 = 'サラダ'
+      var category_name4 = 'スープ'
+
+      itemLists.forEach((value,index) => {
+        if(value.category_id == '01'){
+          itemLists[index].category_id = category_name1
+        }else if (value.category_id == '02'){
+          itemLists[index].category_id = category_name2
+        }else if(value.category_id == '03'){
+          itemLists[index].category_id = category_name3
+        }else {
+          itemLists[index].category_id = category_name4
+        }
+      })
+      // console.log(itemLists)
+      this.desserts = itemLists
+    },
+
+
+    edit(item) {
+      console.log(item.name);
+    },
   }
+}
 </script>
 
 <style scoped>
@@ -159,6 +161,9 @@ import '~/assets/css/style.css'
 }
 .order-title h1{
   font-size: 20px;
+}
+.col{
+  height: 100vh;
 }
 
 
