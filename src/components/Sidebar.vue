1a2d8052cc00674d4371e8aa4f05b0ece17bb7ba
[ujitayuki0329] <[ujitayuki0329@gmail.com]>
2022-07-04 16:35:43 +0900

    各種出力ページレイアウト実装

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -88,7 +88,7 @@
           { title: '注文状況一覧', icon: 'mdi-cart', link: '/orderlist' },
           { title: '商品一覧', icon: 'mdi-food-outline', link: '/itemlist' },
           { title: '売上・顧客管理表', icon: 'mdi-database-outline', link: '/users' },
-          { title: '各種出力', icon: 'mdi-export-variant' },
+          { title: '各種出力', icon: 'mdi-export-variant', link: '/export' },
           { title: '休日管理', icon: 'mdi-calendar-blank' },
           { title: '設定', icon: 'mdi-cog-outline' },
         ],
---
+++ src/pages/export.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="display-wrap">
+  <Sidebar />
+    <div class="right-wrap">
+      <v-row class="fill-height">
+        <v-col>
+          <v-simple-table>
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th class="text-left">
+                    名前
+                  </th>
+                  <!-- <th class="text-left">
+                    Calories
+                  </th> -->
+                  <th class="text-left">
+                    PDF出力
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>注文状況一覧(日単位)</td>
+                  <td>
+                    <div class="my-2">
+                      <v-btn
+                        color="warning"
+                        dark
+                      >
+                        PDF出力
+                      </v-btn>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>注文状況一覧(週単位)</td>
+                  <td>
+                    <div class="my-2">
+                      <v-btn
+                        color="warning"
+                        dark
+                      >
+                        PDF出力
+                      </v-btn>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>商品一覧</td>
+                  <td>
+                    <div class="my-2">
+                      <v-btn
+                        color="warning"
+                        dark
+                      >
+                        PDF出力
+                      </v-btn>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>事前商品登録一覧</td>
+                  <td>
+                    <div class="my-2">
+                      <v-btn
+                        color="warning"
+                        dark
+                      >
+                        PDF出力
+                      </v-btn>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>売上一覧</td>
+                  <td>
+                    <div class="my-2">
+                      <v-btn
+                        color="warning"
+                        dark
+                      >
+                        PDF出力
+                      </v-btn>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>顧客一覧</td>
+                  <td>
+                    <div class="my-2">
+                      <v-btn
+                        color="warning"
+                        dark
+                      >
+                        PDF出力
+                      </v-btn>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+         
+         
+        </v-col>
+      </v-row>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from '~/components/Sidebar'
+import '~/assets/css/style.css'
+
+  export default {
+    head() {
+      return {
+        title: '各種出力 | smartfoodlocker管理画面'
+      }
+    },
+    components: {
+      Sidebar,
+    },
+    data () {
+    return {
+      
+      
+    }
+  },
+  methods: {
+    
+    
+  }
+    
+  }
+</script>
+
+<style scoped>
+td {
+  font-weight: bold;
+  color: gray;
+}
+button{
+  font-weight: bold;
+}
+
+
+</style>
