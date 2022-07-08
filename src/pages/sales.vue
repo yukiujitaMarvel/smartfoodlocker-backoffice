6f9c7877daee1b19d82df6d6db3cb0d9263bb2b1
[ujitayuki0329] <[ujitayuki0329@gmail.com]>
2022-07-08 10:47:43 +0900

    売上一覧データ表示制御

--- src/pages/sales.vue
+++ src/pages/sales.vue
@@ -4,7 +4,7 @@
     <div class="right-wrap">
       <v-row class="fill-height">
         <v-col>
-          <v-card v-if="users">
+          <v-card>
             <div class="order-title">
               <h1>売上一覧</h1>
             </div>
@@ -31,66 +31,22 @@
                   PDF出力
                 </v-btn>
               </div>
-
             </v-card-title>
-            
             <v-data-table
               :headers="headers"
-              :items="users"
-              :items-per-page="5"
+              :items="desserts"
               :search="search"
-              class="elevation-1"
             >
-            
-              <template v-slot:top>
-                <v-dialog v-model="dialog" max-width="500px">
-                  <v-card>
-                    <v-card-title>
-                      <span class="headline">ユーザー編集</span>
-                    </v-card-title>
-                    <v-card-text>
-                      <v-container>
-                        <v-row>
-                          <v-col cols="6">
-                            <v-text-field v-model="user.email" label="メールアドレス" />
-                          </v-col>
-                          <v-col cols="6">
-                            <v-text-field v-model="user.name" label="名前" />
-                          </v-col>
-                          <v-col cols="12">
-                            <v-text-field v-model="user.memo" label="メモ" />
-                          </v-col>
-                        </v-row>
-                      </v-container>
-                    </v-card-text>
-                    <v-card-actions>
-                      <v-spacer />
-                      <v-btn @click="close">閉じる</v-btn>
-                      <v-btn v-if="isPersistedUser" class="primary" @click="update">更新する</v-btn>
-                      <v-btn v-else class="primary" @click="create">追加する</v-btn>
-                      <v-spacer />
-                    </v-card-actions>
-                  </v-card>
-                </v-dialog>
-              </template>
-              
-              <template v-slot:[`item.actions`]="{ item }">
-                <v-icon
-                  small
-                  @click="edit(item)"
-                >
-                  mdi-pencil
-                </v-icon>
-                <v-icon
-                  small
-                  @click="remove(item)"
-                >
-                  mdi-delete
-                </v-icon>
-              </template>
+            <template v-slot:[`item.actions`]="{ item }">
+              <v-icon
+                nomal
+                @click="edit(item)"
+              >
+                mdi-dots-vertical
+              </v-icon>
+            </template>
             </v-data-table>
           </v-card>
-         
         </v-col>
       </v-row>
     </div>
@@ -98,72 +54,72 @@
 </template>
 
 <script>
+import { API, graphqlOperation} from 'aws-amplify'
+import { listOrders } from '../graphql/queries'
 import Sidebar from '~/components/Sidebar'
 import '~/assets/css/style.css'
 
   export default {
     head() {
       return {
-        title: '売上一覧 | smartfoodlocker管理画面'
+        title: '注文一覧 | smartfoodlocker管理画面'
       }
     },
     components: {
       Sidebar,
     },
     data () {
-    return {
-      dialog: false,
-      search: '',
-      headers: [
-        { text: 'ID', value: 'id' },
-        { text: 'メールアドレス', value: 'email' },
-        { text: '名前', value: 'name' },
-        { text: 'メモ', value: 'memo' },
-        { text: '操作', value: 'actions' }
-      ],
-      user: {},
+      return {
+        search: '',
+        headers: [
+          {
+            text: '注文番号',
+            align: 'start',
+            filterable: false,
+            value: 'id',
+          },
+          { text: '注文詳細', value: 'order_detail' },
+          { text: '商品数', value: 'item_num' },
+          { text: '顧客番号', value: 'user_id' },
+          { text: '注文日時', value: 'createdAt' },
+          { text: '売上', value: 'earning' },
+          { text: '操作', value: 'actions' },
+        ],
+        desserts: [],
+      }
+    },
+    async created() {
+      await this.getOrders()
+    },
+    methods: {
+      async getOrders() {
+        const orders = await API.graphql(graphqlOperation(listOrders));
+        const orderLists = orders.data.listOrders.items;
+
+        const earningItem = orderLists.filter((value) => {
+          return value.statas == '03';
+        })
+
+        var status1 = '準備中'
+        var status2 = 'キャンセル'
+        var status3 = '完了'
+
+        earningItem.forEach((value, index) => {
+          if(value.statas == '01') {
+            earningItem[index].statas = status1
+          }else if(value.statas == '02') {
+            earningItem[index].statas = status2
+          }else {
+            earningItem[index].statas = status3
+          }
+        })
+
+        this.desserts = earningItem
+      },
+      edit(item) {
+        console.log(item.name)
+      }
     }
-  },
-  computed: {
-    users () {
-      return this.$store.getters['getUsers']
-    },
-    // isPersistedUser () {
-    //   return !!this.user.id
-    // },
-    // formTitle () {
-    //   return this.isPersistedUser ? 'ユーザー編集' : 'ユーザー追加'
-    // }
-  },
-  methods: {
-    add (user) {
-      this.user = {}
-      this.dialog = true
-    },
-    create () {
-      const payload = { user: this.user }
-      this.$store.commit('addUser', payload)
-      this.close()
-    },
-    edit (user) {
-      this.user = Object.assign({}, user)
-      this.dialog = true
-    },
-    update () {
-      const payload = { user: this.user }
-      this.$store.commit('updateUser', payload)
-      this.close()
-    },
-    remove (user) {
-      const payload = { user: user }
-      this.$store.commit('removeUser', payload)
-    },
-    close () {
-      this.dialog = false
-      this.user = {}
-    },
-  }
-    
   }
 </script>
 
@@ -174,6 +130,9 @@ import '~/assets/css/style.css'
 .order-title h1{
   font-size: 20px;
 }
-
+.order-title p{
+  font-size: 14px;
+  margin-top: 10px;
+}
 
 </style>
