a166268d1a04a4725241101f12cde7c64f62c3e4
[ujitayuki0329] <[ujitayuki0329@gmail.com]>
2022-07-08 15:06:50 +0900

    チケット登録画面レイアウト実装

--- src/pages/ticket.vue
+++ src/pages/ticket.vue
@@ -38,7 +38,7 @@
                   >
                     <v-icon>mdi-close</v-icon>
                   </v-btn>
-                  <v-toolbar-title>休日設定</v-toolbar-title>
+                  <v-toolbar-title>チケット登録</v-toolbar-title>
                   <v-spacer></v-spacer>
                   <v-toolbar-items>
                     <v-btn
@@ -54,141 +54,40 @@
                   three-line
                   subheader
                 >
-                  <!-- カレンダー -->
-                  <template>
-                    <v-row class="fill-height">
-                      <v-col>
-                        <v-sheet height="64">
-                          <v-toolbar
-                            flat
-                          >
-                            <v-btn
-                              outlined
-                              class="mr-4"
-                              color="grey darken-2"
-                              @click="setToday"
-                            >
-                              Today
-                            </v-btn>
-                            <v-btn
-                              fab
-                              text
-                              small
-                              color="grey darken-2"
-                              @click="prev"
-                            >
-                              <v-icon small>
-                                mdi-chevron-left
-                              </v-icon>
-                            </v-btn>
-                            <v-btn
-                              fab
-                              text
-                              small
-                              color="grey darken-2"
-                              @click="next"
-                            >
-                              <v-icon small>
-                                mdi-chevron-right
-                              </v-icon>
-                            </v-btn>
-                            <v-toolbar-title v-if="$refs.calendar">
-                              {{ $refs.calendar.title }}
-                            </v-toolbar-title>
-                            <v-spacer></v-spacer>
-                            <v-menu
-                              bottom
-                              right
-                            >
-                              <template v-slot:activator="{ on, attrs }">
-                                <v-btn
-                                  outlined
-                                  color="grey darken-2"
-                                  v-bind="attrs"
-                                  v-on="on"
-                                >
-                                  <span>{{ typeToLabel[type] }}</span>
-                                  <v-icon right>
-                                    mdi-menu-down
-                                  </v-icon>
-                                </v-btn>
-                              </template>
-                              <v-list>
-                                <v-list-item @click="type = 'day'">
-                                  <v-list-item-title>Day</v-list-item-title>
-                                </v-list-item>
-                                <v-list-item @click="type = 'week'">
-                                  <v-list-item-title>Week</v-list-item-title>
-                                </v-list-item>
-                                <v-list-item @click="type = 'month'">
-                                  <v-list-item-title>Month</v-list-item-title>
-                                </v-list-item>
-                                <v-list-item @click="type = '4day'">
-                                  <v-list-item-title>4 days</v-list-item-title>
-                                </v-list-item>
-                              </v-list>
-                            </v-menu>
-                          </v-toolbar>
-                        </v-sheet>
-                        <v-sheet height="600">
-                          <v-calendar
-                            ref="calendar"
-                            v-model="focus"
-                            color="primary"
-                            :events="events"
-                            :event-color="getEventColor"
-                            :type="type"
-                            @click:event="showEvent"
-                            @click:more="viewDay"
-                            @click:date="viewDay"
-                            @change="updateRange"
-                          ></v-calendar>
-                          <v-menu
-                            v-model="selectedOpen"
-                            :close-on-content-click="false"
-                            :activator="selectedElement"
-                            offset-x
-                          >
-                            <v-card
-                              color="grey lighten-4"
-                              min-width="350px"
-                              flat
-                            >
-                              <v-toolbar
-                                :color="selectedEvent.color"
-                                dark
-                              >
-                                <v-btn icon>
-                                  <v-icon>mdi-pencil</v-icon>
-                                </v-btn>
-                                <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
-                                <v-spacer></v-spacer>
-                                <v-btn icon>
-                                  <v-icon>mdi-heart</v-icon>
-                                </v-btn>
-                                <v-btn icon>
-                                  <v-icon>mdi-dots-vertical</v-icon>
-                                </v-btn>
-                              </v-toolbar>
-                              <v-card-text>
-                                <span v-html="selectedEvent.details"></span>
-                              </v-card-text>
-                              <v-card-actions>
-                                <v-btn
-                                  text
-                                  color="secondary"
-                                  @click="selectedOpen = false"
-                                >
-                                  Cancel
-                                </v-btn>
-                              </v-card-actions>
-                            </v-card>
-                          </v-menu>
-                        </v-sheet>
-                      </v-col>
-                    </v-row>
-                  </template>
-                  <!-- カレンダー終了 -->
+                  <v-card>
+                    <v-card-text>
+                      <div>
+                        <v-text-field
+                          label="チケット名"
+                          hide-details="auto"
+                        ></v-text-field>
+                        <div class="file-wrap">
+                          <label for="form-image">ファイルを選択</label>
+                          <input type="file" id="form-image">
+                          <!-- <div class="upload-img" v-if="postData.thumbnail != ''">
+                            <img :src="postData.thumbnail" alt="">
+                          </div> -->
+                          <span class="select-image">選択されていません</span>
+                        </div>
+                        <v-text-field
+                          label="タイトル"
+                          hide-details="auto"
+                        ></v-text-field>
+                        <v-text-field
+                          label="内容"
+                          hide-details="auto"
+                        ></v-text-field>
+                        <div class="use-ticket-wrap">
+                          <h1>使用開始日</h1>
+                          <input type="date" name="example" value="2022-05-20">
+                        </div>
+                        <div class="expiry-ticket-wrap">
+                          <h1>有効期限</h1>
+                          <input type="date" name="example" value="2022-05-20">
+                        </div>
+                      </div>
+                    </v-card-text>
+                  </v-card>
                 </v-list>
               </v-card>
             </v-dialog>
@@ -335,67 +234,7 @@ import '~/assets/css/style.css'
     }
   },
   methods: {
-      viewDay ({ date }) {
-        this.focus = date
-        this.type = 'day'
-      },
-      getEventColor (event) {
-        return event.color
-      },
-      setToday () {
-        this.focus = ''
-      },
-      prev () {
-        this.$refs.calendar.prev()
-      },
-      next () {
-        this.$refs.calendar.next()
-      },
-      showEvent ({ nativeEvent, event }) {
-        const open = () => {
-          this.selectedEvent = event
-          this.selectedElement = nativeEvent.target
-          requestAnimationFrame(() => requestAnimationFrame(() => this.selectedOpen = true))
-        }
-
-        if (this.selectedOpen) {
-          this.selectedOpen = false
-          requestAnimationFrame(() => requestAnimationFrame(() => open()))
-        } else {
-          open()
-        }
-
-        nativeEvent.stopPropagation()
-      },
-      updateRange ({ start, end }) {
-        const events = []
-
-        const min = new Date(`${start.date}T00:00:00`)
-        const max = new Date(`${end.date}T23:59:59`)
-        const days = (max.getTime() - min.getTime()) / 86400000
-        const eventCount = this.rnd(days, days + 20)
-
-        for (let i = 0; i < eventCount; i++) {
-          const allDay = this.rnd(0, 3) === 0
-          const firstTimestamp = this.rnd(min.getTime(), max.getTime())
-          const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-          const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
-          const second = new Date(first.getTime() + secondTimestamp)
-
-          events.push({
-            name: this.names[this.rnd(0, this.names.length - 1)],
-            start: first,
-            end: second,
-            color: this.colors[this.rnd(0, this.colors.length - 1)],
-            timed: !allDay,
-          })
-        }
-
-        this.events = events
-      },
-      rnd (a, b) {
-        return Math.floor((b - a + 1) * Math.random()) + a
-      },
+      
     },
   }
 </script>
@@ -420,5 +259,41 @@ button{
 .order-title h1{
   font-size: 20px;
 }
-
+.use-ticket-wrap{
+  margin-top: 10px;
+  padding-top: 8px;
+  border-bottom: 1px solid rgba(105, 103, 103, 0.87);
+}
+.use-ticket-wrap h1{
+  font-size: 16px;
+  padding-bottom: 5px;
+  color: rgba(136, 133, 133, 0.87);
+}
+.expiry-ticket-wrap{
+  margin-top: 10px;
+  padding-top: 8px;
+}
+.expiry-ticket-wrap h1{
+  font-size: 16px;
+  padding-bottom: 5px;
+  color: rgba(136, 133, 133, 0.87);
+}
+.file-wrap{
+  margin: 30px 0 30px 0;
+}
+label {
+  padding: 5px 20px;
+  color: #ffffff;
+  background-color: orange;
+  cursor: pointer;
+  border-radius:10px;
+  transition: .3s;
+  font-weight: bold;
+}
+label:hover {
+  opacity: 0.8;
+}
+input[type="file"] {
+  display: none;
+}
 </style>
