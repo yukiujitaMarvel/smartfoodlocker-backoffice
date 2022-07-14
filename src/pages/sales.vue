d58856bf7c16c7758fbea9bff53e15f9084ca083
[ujitayuki0329] <[ujitayuki0329@gmail.com]>
2022-07-14 10:30:26 +0900

    売上一覧追加レイアウト実装

--- src/pages/sales.vue
+++ src/pages/sales.vue
@@ -8,6 +8,20 @@
             <div class="order-title">
               <h1>売上一覧</h1>
             </div>
+            <div class="salse">
+              <div class="salse-wrap dairy-salse-wrap">
+                <h1>DAIRY</h1>
+                <div class="dairy">
+                  <p>¥239,000</p>
+                </div>
+              </div>
+              <div class="salse-wrap">
+                <h1>MONTHRY</h1>
+                <div class="monthry">
+                  <p>¥1,239,000</p>
+                </div>
+              </div>
+            </div>
             <v-card-title>
               <v-text-field
                 v-model="search"
@@ -78,12 +92,12 @@ import '~/assets/css/style.css'
             filterable: false,
             value: 'id',
           },
-          { text: '注文詳細', value: 'order_detail' },
-          { text: '商品数', value: 'item_num' },
+          // { text: '注文詳細', value: 'order_detail' },
+          // { text: '商品数', value: 'item_num' },
           { text: '顧客番号', value: 'user_id' },
+          { text: '売上', value: 'total_price' },
           { text: '注文日時', value: 'createdAt' },
-          { text: '売上', value: 'earning' },
-          { text: '操作', value: 'actions' },
+          // { text: '操作', value: 'actions' },
         ],
         desserts: [],
       }
@@ -133,6 +147,41 @@ import '~/assets/css/style.css'
 .order-title p{
   font-size: 14px;
   margin-top: 10px;
+}
+.salse{
+  display: flex;
+  padding: 10px;
+}
+.dairy{
+  border: 3px solid orange ;
+  text-align: right;
+}
+.dairy p {
+  color: orange;
+  font-size: 30px;
+  margin: 0;
+  padding: 10px;
+}
+.monthry{
+  border: 3px solid #EA5303;
+  text-align: right;
+}
+.monthry p {
+  color: #EA5303;
+  font-size: 30px;
+  margin: 0;
+  padding: 10px;
+}
+.salse-wrap {
+  width: calc(100%/2);
+  margin-right: 20px;
+}
+.salse-wrap h1 {
+  font-size: 12px;
+  color: #EA5303;
+}
+.dairy-salse-wrap h1{
+  color: orange;
 }
 
 </style>
