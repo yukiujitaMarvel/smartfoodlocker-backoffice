1325ad1f385e5a579f1a964b49e66906e6063493
Eri Yajima <eri.y@marvel.engineer>
2022-07-22 14:40:35 +0900

    yajima_0715 仕様、デザイン実装完了

--- src/pages/setting.vue
+++ src/pages/setting.vue
@@ -3,81 +3,170 @@
   <Sidebar />
     <v-card class="right-wrap" >
       <h1>設定</h1>
-      <div>
-        <p class="subTitle"><v-icon>mdi-account</v-icon>登録情報</p>
-        <div>
-          <v-row>
-            <v-col
-              cols="12"
-              sm="6"
-              md="4"
+      <div class="saveBtn">
+        <v-btn
+          color="secondary"
+          dark
+          large
+        >
+          保存
+        </v-btn>
+      </div>
+      <h2 class="subTitle"><v-icon>mdi-account</v-icon>登録情報</h2>
+      <div class="accounInf">
+        <v-row>
+          <v-col
+            cols="12"
+            sm="6"
+            md="4"
+          >
+            <v-text-field
+            dense
+            label="企業名"
             >
-              <v-text-field
-              dense
-              label="企業名"
-              >
-              </v-text-field>
-            </v-col>
-            <v-col
-              cols="12"
-              sm="6"
-              md="4"
+            </v-text-field>
+          </v-col>
+          <v-col
+            cols="12"
+            sm="6"
+            md="4"
+          >
+            <v-text-field
+            dense
+            label="ユーザー名"
             >
-              <v-text-field
-              dense
-              label="ユーザー名"
-              >
-              </v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col
-              cols="12"
-              sm="6"
-              md="8"
+            </v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col
+            cols="12"
+            sm="6"
+            md="8"
+          >
+            <v-text-field
+            dense
+            label="住所"
             >
-              <v-text-field
-              dense
-              label="住所"
-              >
-              </v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col
-              cols="12"
-              sm="6"
-              md="4"
+            </v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col
+            cols="12"
+            sm="6"
+            md="4"
+          >
+            <v-text-field
+            dense
+            label="電話番号"
             >
-              <v-text-field
-              dense
-              label="電話番号"
-              >
-              </v-text-field>
-            </v-col>
-            <v-col
-              cols="12"
-              sm="6"
-              md="4"
+            </v-text-field>
+          </v-col>
+          <v-col
+            cols="12"
+            sm="6"
+            md="4"
+          >
+            <v-text-field
+            dense
+            label="パスワード"
             >
-              <v-text-field
-              dense
-              label="パスワード"
-              >
-              </v-text-field>
-            </v-col>
-          </v-row>
-        </div>
+            </v-text-field>
+          </v-col>
+        </v-row>
       </div>
       <div>
-        <p class="subTitle"><v-icon>mdi-av-timer</v-icon>提供時間情報</p>
+        <h2 class="subTitle"><v-icon>mdi-av-timer</v-icon>提供日時情報</h2>
         <div class="weekTime">
-          <v-row>
-            <v-col 
-            v-for="align in alignments"
-            :key="align"
-            :align-self="align"
-            >
+          <p>定休日</p>
+          <v-row class="dayBtn">
+            <v-col>
+              <v-btn class="daySetting"
+                fab
+                small
+                v-on:click='isSelected01=!isSelected01'
+                v-bind:class='{active:isSelected01}'
+              >
+                月
+              </v-btn>
+            </v-col>
+            <v-col>
+              <v-btn class="daySetting"
+                fab
+                small
+                v-on:click='isSelected02=!isSelected02'
+                v-bind:class='{active:isSelected02}'
+              >
+                火
+              </v-btn>
+            </v-col>
+            <v-col>
+              <v-btn class="daySetting"
+                fab
+                small
+                v-on:click='isSelected03=!isSelected03'
+                v-bind:class='{active:isSelected03}'
+              >
+                水
+              </v-btn>
+            </v-col>
+            <v-col>
+              <v-btn class="daySetting"
+                fab
+                small
+                v-on:click='isSelected04=!isSelected04'
+                v-bind:class='{active:isSelected04}'
+              >
+                木
+              </v-btn>
+            </v-col>
+            <v-col>
+              <v-btn class="daySetting"
+                fab
+                small
+                v-on:click='isSelected05=!isSelected05'
+                v-bind:class='{active:isSelected05}'
+              >
+                金
+              </v-btn>
+            </v-col>
+            <v-col>
+              <v-btn class="daySetting"
+                fab
+                small
+                v-on:click='isSelected06=!isSelected06'
+                v-bind:class='{active:isSelected06}'
+              >
+                土
+              </v-btn>
+            </v-col>
+            <v-col>
+              <v-btn class="daySetting"
+                fab
+                small
+                v-on:click='isSelected07=!isSelected07'
+                v-bind:class='{active:isSelected07}'
+              >
+                日
+              </v-btn>
+            </v-col>
+            <!-- <v-col>
+              <v-btn class="daySetting"
+                fab
+                small
+                v-on:click='isSelected08=!isSelected08'
+                v-bind:class='{active:isSelected08}'
+              >
+                祝
+              </v-btn>
+            </v-col> -->
+          </v-row>
+          <p>提供時間</p>
+          <v-row class="timeSetting"
+            v-bind:class='{active:isSelected01}'
+          >
+            <v-col>
               月曜日
             </v-col>
             <v-col>
@@ -112,13 +201,12 @@
               </template>
             </v-col>
           </v-row>
-          <v-row>
-            <v-col 
-            v-for="align in alignments"
-            :key="align"
-            :align-self="align"
-            >
-            火曜日</v-col>
+          <v-row class="timeSetting"
+            v-bind:class='{active:isSelected02}'
+          >
+            <v-col>
+              火曜日
+            </v-col>
             <v-col>
               <template>
                 <vue-timepicker
@@ -151,12 +239,10 @@
               </template>
             </v-col>
           </v-row>
-          <v-row>
-            <v-col 
-            v-for="align in alignments"
-            :key="align"
-            :align-self="align"
-            >
+          <v-row class="timeSetting"
+            v-bind:class='{active:isSelected03}'
+          >
+            <v-col>
               水曜日
             </v-col>
             <v-col>
@@ -191,12 +277,10 @@
               </template>
             </v-col>
           </v-row>
-          <v-row>
-            <v-col 
-            v-for="align in alignments"
-            :key="align"
-            :align-self="align"
-            >
+          <v-row class="timeSetting"
+            v-bind:class='{active:isSelected04}'
+          >
+            <v-col>
               木曜日
             </v-col>
             <v-col>
@@ -231,12 +315,10 @@
               </template>
             </v-col>
           </v-row>
-          <v-row>
-            <v-col 
-            v-for="align in alignments"
-            :key="align"
-            :align-self="align"
-            >
+          <v-row class="timeSetting"
+            v-bind:class='{active:isSelected05}'
+          >
+            <v-col>
               金曜日
             </v-col>
             <v-col>
@@ -271,12 +353,10 @@
               </template>
             </v-col>
           </v-row>
-          <v-row>
-            <v-col 
-            v-for="align in alignments"
-            :key="align"
-            :align-self="align"
-            >
+          <v-row class="timeSetting"
+            v-bind:class='{active:isSelected06}'
+          >
+            <v-col>
               土曜日
             </v-col>
             <v-col>
@@ -311,12 +391,10 @@
               </template>
             </v-col>
           </v-row>
-          <v-row>
-            <v-col 
-            v-for="align in alignments"
-            :key="align"
-            :align-self="align"
-            >
+          <v-row class="timeSetting"
+            v-bind:class='{active:isSelected07}'
+          >
+            <v-col>
               日曜日
             </v-col>
             <v-col>
@@ -351,12 +429,10 @@
               </template>
             </v-col>
           </v-row>
-          <v-row>
-            <v-col 
-            v-for="align in alignments"
-            :key="align"
-            :align-self="align"
-            >
+          <!-- <v-row class="timeSetting"
+            v-bind:class='{active:isSelected08}'
+          >
+            <v-col>
               祝日
             </v-col>
             <v-col>
@@ -390,7 +466,7 @@
                 </vue-timepicker>
               </template>
             </v-col>
-          </v-row>
+          </v-row> -->
         </div>
       </div>
     </v-card>
@@ -417,25 +493,18 @@ import 'vue2-timepicker/dist/VueTimepicker.css'
     components: {
       'vue-timepicker': VueTimepicker,
     },
-    data: () => ({
-      alignments: [
-        'center',
-      ]
-    }),
-  // data () {
-  //   return {
-  //     dialog: false,
-  //     search: '',
-  //     headers: [
-  //       { text: 'ユーザーID', value: 'user_id' },
-  //       { text: '名前', value: 'user_name' },
-  //       { text: 'メールアドレス', value: 'user_email' },
-  //       { text: '電話番号', value: 'user_number' },
-  //       // { text: '最終更新', value: 'updatedAt' }
-  //     ],
-  //     user: [],
-  //   }
-  // },
+    data(){
+      return {
+        isSelected01: false,
+        isSelected02: false,
+        isSelected03: false,
+        isSelected04: false,
+        isSelected05: false,
+        isSelected06: false,
+        isSelected07: false
+        // isSelected08: false
+      }
+    },
 
 
   async created() {
@@ -456,34 +525,57 @@ import 'vue2-timepicker/dist/VueTimepicker.css'
   padding: 20px 20px;
 }
 
+.saveBtn {
+  margin: 20px 0;
+}
+
 .subTitle {
   margin-top: 20px;
   color: #EA5303;
   font-weight: bold;
   display: flex;
   align-items: center;
+  font-size: 20px ;
 }
 
-.mdi-account::before , .mdi-av-timer::before {
-  color: #EA5303;
+.accounInf {
+  margin: 20px 15px;
+  font-size: 14px !important;
 }
 
 .weekTime{
   max-width: 60%;
+  margin: 0 auto 30px 15px;
 }
-
+.mdi-account::before , .mdi-av-timer::before {
+  color: #EA5303;
+  margin-right: 5px;
+}
+.weekTime p {
+  font-weight: bold;
+  font-size: 17px;
+  margin-top: 20px;
+}
+.dayBtn {
+  margin-left: 15px;
+}
+.daySetting.active.theme--light.v-btn.v-btn--has-bg{
+  background-color: #14B855 !important;
+  color: #fff;
+}
+.timeSetting {
+  margin-left: 15px;
+}
+.timeSetting.active {
+  opacity: 0.5;
+  pointer-events: none;
+}
 .center {
   display: flex;
   align-items: center;
   justify-content: center;
   padding: 0 !important;
   max-width: 20px !important;
-}
-
-.my-time-picker >>> .vue__time-picker .dropdown ul li:not([disabled]).active,
-.my-time-picker >>> .vue__time-picker .dropdown ul li:not([disabled]).active:focus,
-.my-time-picker >>> .vue__time-picker .dropdown ul li:not([disabled]).active:hover {
-  background: #999;
 }
 
 </style>
